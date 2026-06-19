6e2259f9ea8cbe832f5a67b425ea906c4eff6a11
aryanbhashkar-tech <aryan.bhashkar@difm.tech>
2026-06-20 00:57:25 +0530

    feat: enhance rate limiter to track requests by IP address

--- rate-limitter/middleware.js
+++ rate-limitter/middleware.js
@@ -2,26 +2,31 @@ import { redis } from "./lib/rate-limiter.js";
 
 export const rateMiddleware = async (req, res, next) => {
     try {
-        const curr_count = await redis.get('count');
-        const startedAtStr = await redis.get('time');
-        if (!curr_count || !startedAtStr) {
-            await redis.setnx('count', 0);
-            await redis.setnx('time', Date.now().toString());
-            next();
+        const ip=req.ip;
+        console.log("ip == ",ip)
+        const curr_count = await redis.get(`rl-${ip}-count`);
+        const startedAtStr = await redis.get(`rl-${ip}-time`);
+        if (!curr_count) {
+            await redis.setnx(`rl-${ip}-count`, 0);
+        }
+        if (!startedAtStr) {
+            await redis.setnx(`rl-${ip}-time`, Date.now().toString());
         }
         const startedAt = parseInt(startedAtStr, 10);
         const now = Date.now();
         const elapsedMs = now - startedAt;
         const elapsedSeconds = Math.floor(elapsedMs / 1000);
-        console.log("elapsedMs from the middleware ", elapsedMs);
-        console.log("current count === ", parseInt(curr_count, 10));
-        if (parseInt(curr_count, 10) >= 10) {
-            return res.status(403).json({ msg: "hitting more than y deserve" });
+        console.log("seconds == ", elapsedSeconds);
+        console.log("count == ", parseInt(curr_count, 10));
+        if (parseInt(curr_count, 10) >= 10 && elapsedSeconds < 60) {
+            return res.status(429).json({ msg: "hitting more than y deserve" });
         }
         if (elapsedSeconds > 60) {
-            await redis.set('time', Date.now().toString());
-            await redis.set('count', 0);
+            await redis.set(`rl-${ip}-time`, Date.now().toString());
+            await redis.set(`rl-${ip}-count`, 0);
+            // return res.status(429).json({msg:"limit resets"});
         }
+        await redis.incr(`rl-${ip}-count`);
         next();
     } catch (e) {
         next(e);
